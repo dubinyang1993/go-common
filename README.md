eb0bb060f3265a97b3e5891d51ade5c771467874
dubinyang47 <dubinyang47@163.com>
2023-05-05 14:30:24 +0800

    update go-common code

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
+# go-common
 下载安装：
     
     go get -u github.com/dubinyang1993/go-common
 
 内容介绍：
 
-    读取配置文件;
-
-    初始化 mysql;
-
-    初始化 redis;
+    常用工具类
--- main.go
+++ main.go
@@ -2,20 +2,8 @@ package main
 
 import (
 	"fmt"
-	"go-common/config"
-	"go-common/mysql"
-	"go-common/redis"
 )
 
 func main() {
-	fmt.Println("go-common is starting...")
-
-	//配置文件和日志
-	config.Init()
-
-	//初始化数据库
-	mysql.Init()
-
-	//初始化Redi
-	redis.Init()
+	fmt.Println("go-common")
 }
